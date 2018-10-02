a25641a8f8f347a760484ecc8d64077dfd072506
onestraw <hexiaowei91@163.com>
2018-10-02 10:01:48 +0800

    Add dictionary
    Change-Id: I038508110c485c22f23e9628c36d3d6d6b0a4cca

---
+++ tools/lookup.py
@@ -0,0 +1,81 @@
+# coding=utf-8
+
+import os
+import json
+
+
+RawWordJson = 'data/word.json'
+WordIndexJson = 'data/word_index.json'
+
+
+class State(object):
+    # state machine
+    BlockEnd = 1
+    BlockBegin = 2
+    BeforeWordBegin = 3
+    Word = 4
+    WordEnd = 5
+
+
+def create_index(json_file, index_file):
+    f = open(json_file)
+    body = f.read()
+    f.close()
+
+    # key: [offset, limit]
+    index = {}
+    word = []
+    offset = -1
+    limit = -1
+
+    state = State.BlockEnd
+    for i, c in enumerate(body):
+        if state == State.BlockEnd:
+            if c == '{':
+                offset = i
+                state = State.BlockBegin
+        elif state == State.BlockBegin:
+            if c == ':':
+                state = State.BeforeWordBegin
+        elif state == State.BeforeWordBegin:
+            if c == '"':
+                state = State.Word
+        elif state == State.Word:
+            if c == '"':
+                state = State.WordEnd
+            else:
+                word.append(c)
+        elif state == State.WordEnd:
+            if c == '}':
+                limit = i - offset + 1
+                index[''.join(word)] = [offset, limit]
+                word = []
+                state = State.BlockEnd
+
+    f = open(index_file, 'w+')
+    f.write(json.dumps(index))
+    f.close()
+
+
+def lookup(word):
+    if not os.path.exists(WordIndexJson):
+        create_index(RawWordJson, WordIndexJson)
+    f = open(WordIndexJson)
+    index = json.loads(f.read())
+    f.close()
+
+    if word not in index:
+        return ''
+
+    offset, limit = index[word]
+    f = open(RawWordJson)
+    f.seek(offset)
+    record = f.read(limit)
+    f.close()
+    data = json.loads(record)
+    return data['explanation']
+
+
+if __name__=='__main__':
+    w = u'天'
+    print(lookup(w))
